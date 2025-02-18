63013bd8f54a30a4d691a1002ec22771c9d82e63
lakshmi-unni <lakshmiunnioffcial@gmail.com>
2025-02-18 23:55:52 +0530

    add all models

--- src/Components/example1/Example1.js
+++ src/Components/example1/Example1.js
@@ -5,14 +5,14 @@ import Triangles from './Triangles';
 
 const Example1 = () => {
   return (
-    <div className="w-screen h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
+    <div className="w-screen h-screen bg-gradient-to-r from-gray-500 to-black flex items-center justify-center">
       <Canvas camera={{ position: [0, 0, 5] }} className="w-full h-full">
         {/* Ambient Light */}
         <ambientLight intensity={0.5} />
         {/* Triangle Mesh */}
         <Triangles />
         {/* Orbit Controls */}
-        <OrbitControls enableZoom={true} enableRotate={true} />
+        <OrbitControls enableZoom={true} en enableRotate={true} />
       </Canvas>
     </div>
   );
--- src/Components/example1/Triangles.js
+++ src/Components/example1/Triangles.js
@@ -1,9 +1,59 @@
-import React from 'react';
+// import React from 'react';
+// import * as THREE from 'three';
+
+// const Triangles = () => {
+//   return (
+//     <mesh>
+//       <bufferGeometry attach="geometry" >
+//         <bufferAttribute
+//           attach="attributes-position"
+//           array={new Float32Array([  
+//             0, 1, 0,  // top vertex
+//             -1, -1, 0, // bottom left vertex
+//             1, -1, 0  // bottom right vertex
+//           ])}
+//           itemSize={3}
+//           count={3}
+//         />
+
+//         <bufferAttribute
+//           attach="attributes-color"
+//           array={new Float32Array([  
+//             1, 0, 0,  // red
+//             0, 1, 0,  // green
+//             0, 0, 1   // blue
+//           ])}
+//           itemSize={3}
+//           count={3}
+//         />
+//       </bufferGeometry>
+//       <meshBasicMaterial vertexColors={true} side={THREE.DoubleSide} />
+//     </mesh>
+//   );
+// };
+
+// export default Triangles;
+
+
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 const Triangles = () => {
+  const meshRef = useRef();
+  let angle = 0;
+
+  useFrame(() => {
+    if (meshRef.current) {
+      angle += 0.02;
+      const radius = window.innerWidth < 768 ? 1 : 2;
+      meshRef.current.position.x = Math.cos(angle) * radius;
+      meshRef.current.position.y = Math.sin(angle) * radius;
+    }
+  });
+
   return (
-    <mesh>
+    <mesh ref={meshRef}>
       <bufferGeometry attach="geometry">
         <bufferAttribute
           attach="attributes-position"
